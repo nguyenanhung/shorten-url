8c30938459e9912cf08471b5f24b406b84948be2
713uk13m <713uk13m@713uk13ms-MacBook-Pro.local>
2018-12-27 17:37:07 +0700

    add bitly driver

---
+++ src/Driver/BitlyError.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Project shorten-url.
+ * Created by PhpStorm.
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Date: 2018-12-27
+ * Time: 17:29
+ */
+
+namespace nguyenanhung\ShortenUrl\Driver;
+
+class BitlyError extends \Exception
+{
+}
