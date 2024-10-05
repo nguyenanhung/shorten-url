a34a925f17c0064ce258b3bb1205e1fe5acece78
nguyenanhung <hungna.dev@gmail.com>
2024-10-05 14:49:39 +0700

    Add driver Spoo.me Shorten URL

--- src/RequestTrait.php
+++ src/RequestTrait.php
@@ -21,36 +21,40 @@ trait RequestTrait
     /**
      * Function sendRequest
      *
-     * @author: 713uk13m <dev@nguyenanhung.com>
-     * @time  : 2018-12-27 17:05
-     *
      * @param string $url
      * @param string $data
      * @param string $method
      *
      * @return bool|string
+     * @author: 713uk13m <dev@nguyenanhung.com>
+     * @time  : 2018-12-27 17:05
+     *
      */
     public function sendRequest($url = '', $data = '', $method = 'GET')
     {
-        $endpoint = (!empty($data) && (is_array($data) || is_object($data))) ? $url . '?' . http_build_query($data) : $url;
-        $method   = strtoupper($method);
-        $curl     = curl_init();
+        if ((!empty($data) && (is_array($data) || is_object($data)))) {
+            $endpoint = $url . '?' . http_build_query($data);
+        } else {
+            $endpoint = $url;
+        }
+        $method = strtoupper($method);
+        $curl = curl_init();
         curl_setopt_array($curl, array(
-            CURLOPT_URL            => $endpoint,
-            CURLOPT_RETURNTRANSFER => TRUE,
-            CURLOPT_ENCODING       => "",
-            CURLOPT_MAXREDIRS      => 10,
-            CURLOPT_TIMEOUT        => 30,
-            CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST  => $method,
-            CURLOPT_POSTFIELDS     => "",
-            CURLOPT_HTTPHEADER     => array(),
+            CURLOPT_URL => $endpoint,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => array(),
         ));
         $response = curl_exec($curl);
-        $err      = curl_error($curl);
+        $err = curl_error($curl);
         curl_close($curl);
         if ($err) {
-            return NULL;
+            return null;
         } else {
             return $response;
         }
